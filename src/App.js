3dd5e851820af2908f80305f324d6a314aa22608
Alexander <elktrgtr@gmail.com>
2022-06-19 00:35:06 +0300

    sync, address, balance

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 // import 'react-app-polyfill/stable';
 import React from 'react';
 import './App.css';
-// import { TezosToolkit } from '@taquito/taquito';
+import { TezosToolkit } from '@taquito/taquito';
 import { BeaconWallet } from '@taquito/beacon-wallet';
 
-// const tezos = new TezosToolkit('https://mainnet.api.tez.ie');
+const tezos = new TezosToolkit('https://mainnet.api.tez.ie');
+
+const options = {
+  name: 'MyAwesomeDapp',
+  iconUrl: 'https://tezostaquito.io/img/favicon.svg',
+  preferredNetwork: 'mainnet',
+  // eventHandlers: {
+  //   PERMISSION_REQUEST_SUCCESS: {
+  //     handler: async (data) => {
+  //       console.log('permission data:', data);
+  //     },
+  //   },
+  // },
+};
+const wallet = new BeaconWallet(options);
 
 function Button(props) {
   return (
@@ -12,18 +26,52 @@ function Button(props) {
   )
 }
 
+function Graph(props) {
+  return (
+    <div>{props.balance || '---'}</div>
+  )
+}
+
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      synced: false
+      synced: false,
+      tzAddress: null,
+      balance: null
     }
   }
 
-  sync() {
+  async sync() {
     const synced = this.state.synced;
+    let tzAddress = null;
+    if (!synced) {
+      await wallet.requestPermissions({
+        network: {
+          type: 'mainnet',
+        },
+      });
+      tezos.setWalletProvider(wallet);
+      tzAddress = await wallet.getPKH();
+    } else {
+      wallet.clearActiveAccount();
+    }
+    this.updateBalance(tzAddress);
     this.setState({
-      synced: !synced 
+      synced: !synced,
+      tzAddress: tzAddress
+    });
+  }
+
+  async updateBalance(tzAddress) {
+    let balance;
+    if (tzAddress) {
+      balance = await tezos.tz.getBalance(tzAddress).then((b) => {return(`${b.toNumber() / 1000000} ꜩ`)});
+    } else {
+      balance = null;
+    }
+    this.setState({
+      balance: balance
     });
   }
   
@@ -34,6 +82,8 @@ class App extends React.Component {
           text={this.state.synced ? 'unsync' : 'sync'}
           onClick={() => this.sync()}
         />
+        <br />
+        <Graph balance={this.state.balance} />
       </div>
     );
   }
